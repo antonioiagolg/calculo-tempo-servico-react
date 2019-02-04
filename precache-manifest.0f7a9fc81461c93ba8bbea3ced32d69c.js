67e5a459eea58018b4102cb14ebe6b4023fe17f2
Antonio Iago Lemos Gaspar <antonio.iagolg@gmail.com>
2019-02-04 14:28:11 -0300

    Build to gh-pages

---
+++ precache-manifest.0f7a9fc81461c93ba8bbea3ced32d69c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1566ea7db6478195c321",
+    "url": "/static/js/main.1566ea7d.chunk.js"
+  },
+  {
+    "revision": "49e694355af1b66777bb",
+    "url": "/static/js/1.49e69435.chunk.js"
+  },
+  {
+    "revision": "1566ea7db6478195c321",
+    "url": "/static/css/main.81387637.chunk.css"
+  },
+  {
+    "revision": "01176e1ded967303fcd24ada7575333c",
+    "url": "/index.html"
+  }
+];
